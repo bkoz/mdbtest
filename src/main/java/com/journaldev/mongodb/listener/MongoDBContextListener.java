b4799b416614987e399d99df969e13f523876814
Your Name <you@example.com>
2016-01-10 14:46:02 -0500

    initial

---
+++ src/main/java/com/journaldev/mongodb/listener/MongoDBContextListener.java
@@ -0,0 +1,39 @@
+package com.journaldev.mongodb.listener;
+
+import java.net.UnknownHostException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.mongodb.MongoClient;
+
+@WebListener
+public class MongoDBContextListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		MongoClient mongo = (MongoClient) sce.getServletContext()
+							.getAttribute("MONGO_CLIENT");
+		mongo.close();
+		System.out.println("MongoClient closed successfully");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		try {
+			ServletContext ctx = sce.getServletContext();
+			MongoClient mongo = new MongoClient(
+					// ctx.getInitParameter("MONGODB_HOST"), 
+					System.getenv("DB_PORT_27017_TCP_ADDR"),
+					Integer.parseInt(ctx.getInitParameter("MONGODB_PORT")));
+			System.out.println("DB_PORT_27017_TCP_ADDR = " + System.getenv("DB_PORT_27017_TCP_ADDR"));
+			System.out.println("MongoClient initialized successfully");
+			sce.getServletContext().setAttribute("MONGO_CLIENT", mongo);
+		} catch (UnknownHostException e) {
+			throw new RuntimeException("MongoClient init failed");
+		}
+	}
+
+}
